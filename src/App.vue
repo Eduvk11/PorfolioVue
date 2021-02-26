4d3edc7cae3da1d97d408ee535fba1bc0daacf39
Eduardo <eduvk11@hotmail.com>
2021-02-26 12:02:07 +0100

    PorfolioVue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MiComponente></MiComponente>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+< script >
+import HelloWorld from "./components/HelloWorld.vue";
+import MiComponente from "./components/Micomponente.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    MiComponente
   }
-}
+};
 </script>
 
 <style>
